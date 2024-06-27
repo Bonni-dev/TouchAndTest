53099a283502e5efc85bc97188f35d5c9df138ae
Bononi <53324114+Bonnivaldo@users.noreply.github.com>
2024-06-26 23:07:31 -0300

    Navigation classes

---
+++ app/src/main/java/com/example/touchandtest/navigation/AppNavigationGraph.kt
@@ -0,0 +1,24 @@
+package com.example.touchandtest.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.touchandtest.presentation.view.HomeView
+import com.example.touchandtest.presentation.view.ScreenTestView
+
+@Composable
+fun AppNavigationGraph() {
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = Routes.HOME_SCREEN ){
+
+        composable(Routes.HOME_SCREEN) {
+            HomeView(navController)
+        }
+
+        composable(Routes.TOUCH_TEST_SCREEN) {
+            ScreenTestView()
+        }
+    }
+}
---
+++ app/src/main/java/com/example/touchandtest/navigation/Routes.kt
@@ -0,0 +1,6 @@
+package com.example.touchandtest.navigation
+
+object Routes {
+    const val HOME_SCREEN = "HOME"
+    const val TOUCH_TEST_SCREEN = "TOUCH_TEST"
+}
