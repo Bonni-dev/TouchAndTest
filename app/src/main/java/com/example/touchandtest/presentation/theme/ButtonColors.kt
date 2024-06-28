831bf2940ace9375cfe61c9a5f2107f5e1b545a8
Bononi <53324114+Bonnivaldo@users.noreply.github.com>
2024-06-27 22:05:05 -0300

    Refactor initial button

--- app/src/main/java/com/example/touchandtest/presentation/theme/ButtonColors.kt
+++ app/src/main/java/com/example/touchandtest/presentation/theme/ButtonColors.kt
@@ -3,6 +3,14 @@ package com.example.touchandtest.presentation.theme
 import androidx.compose.material3.ButtonColors
 import androidx.compose.ui.graphics.Color
 
+fun initialButtonColor() =
+    ButtonColors(
+        containerColor = Color.Blue,
+        contentColor = Color.Yellow,
+        disabledContainerColor = Color.Gray,
+        disabledContentColor = Color.Black
+    )
+
 fun initialSquareColor() =
     ButtonColors(
         containerColor = Color.LightGray,
--- app/src/main/java/com/example/touchandtest/presentation/view/HomeView.kt
+++ app/src/main/java/com/example/touchandtest/presentation/view/HomeView.kt
@@ -6,17 +6,23 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
+import com.example.touchandtest.R
 import com.example.touchandtest.navigation.Routes
+import com.example.touchandtest.presentation.theme.initialButtonColor
 
 @Composable
 fun HomeView(navController: NavHostController) {
@@ -29,13 +35,14 @@ fun HomeView(navController: NavHostController) {
         ElevatedButton(
             modifier = Modifier
                 .padding(24.dp)
-                .fillMaxWidth()
-                .height(48.dp),
+                .size(240.dp)
+                .clip(CircleShape),
+            colors = initialButtonColor(),
             onClick = { navController.navigate(Routes.TOUCH_TEST_SCREEN) }
         ) {
             Text(
                 fontSize = 24.sp,
-                text = "INICIAR TESTE"
+                text = stringResource(R.string.test_init_button)
             )
         }
     }
